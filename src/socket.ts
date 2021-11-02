0d531ee93d3e443d38f5a87c9dd0a1a0a799bc32
virgilsisoe <virgilsisoe@gmail.com>
2021-11-02 18:12:33 -0400

    socket initial refactor

--- src/socket.ts
+++ src/socket.ts
@@ -1,115 +1,164 @@
-let net = require('net');
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import * as utils from "./utils";
+import * as os from "os";
 
-const os = require('os');
-const output = vscode.window.createOutputChannel('Nuke Tools');
+let net = require("net");
 
-// Send test message to Server
-export function sendTestMessage() {
+const output = vscode.window.createOutputChannel("Nuke Tools");
+
+/**
+ * Prepare a debug message to send to the socket.
+ *
+ * Message contains some valid python code and will show the user information.
+ *
+ * @returns - data object
+ */
+export function prepareDebugMsg(): object {
     const random = () => Math.round(Math.random() * 10);
     const r1 = random();
     const r2 = random();
-    const sum = r1 * r2;
 
-    const hostname = os.hostname();
-    const username = os.userInfo()['username'];
+    let code = `
+    from __future__ import print_function
+    print("Hostname: ${os.hostname()} User: ${os.userInfo()["username"]}")
+    print("Connected to ${getAddresses()}")
+    print("${r1} * ${r2} =", ${r1 * r2})
+    `;
 
-    // FIXME: such an ugly long string 
+    // make everything a single line python command
+    code = code.trim().replace(/\n/g, ";");
+
     const data = {
-        'file': 'vscode/path/tmp_file.py',
-        'text': `from __future__ import print_function;result = ${sum};print("${hostname} user: ${username}. Connected to ${getAddresses()}. ${r1} * ${r2} =", result)`
+        text: code,
+        file: "vscode/path/tmp_file.py",
     };
 
-    // send simple string text
-    // sendText(data['text']);
-
-    sendText(JSON.stringify(data));
+    return data;
 }
 
-// Get Nuke ini in home directory
-function getNukeIni() {
-
-    const nukeConfigPath = require('path').join(
-        os.homedir(), '.nuke/NukeServerSocket.ini'
-    );
-
-    return nukeConfigPath;
+/**
+ * Send a debug test message to the socket connection.
+ */
+export function sendDebugMessage() {
+    sendText(JSON.stringify(prepareDebugMsg()));
 }
 
-// Establish the connection source. check if manual config is enabled then 
-// check for manual config. if none then fallback on default.
-function checkManualConnection(field: string, config: string): string {
+/**
+ * Get the NukeServerSocket.ini file path.
+ *
+ * The path will be returned even if there is no file.
+ *
+ * @returns - path like string.
+ */
+export function getNukeIni(): string {
+    return require("path").join(os.homedir(), ".nuke/NukeServerSocket.ini");
+}
 
-    if (utils.getConfiguration('network.enableManualConnection')) {
-        const extField = utils.getConfiguration(`network.${config}`);
+/**
+ * Get the manual address by looking in the configuration network property.
+ *
+ * Enable manual address could be enabled, but property could be empty, in that
+ * case will show an error message to user and return the `defaultValue`.
+ *
+ * @param property - property name (port, host) to get from the configuration.
+ * @param defaultValue - a default value to fallback in case property is undefined.
+ * @returns - the property value.
+ */
+function getManualAddress(property: string, defaultValue: string): string {
+    const manualAddress = utils.getConfig(`network.${property}`);
 
-        if (!extField) {
-            const manualErrorMsg = `
-            You have enabled Manual Connection but \`Network: #\` 
-            configuration appears to be empty. Falling back on default.
+    if (!manualAddress) {
+        const manualErrorMsg = `
+        You have enabled Manual Connection but "Network: #"
+        configuration appears to be empty. Falling back on default.
+        `;
+        vscode.window.showErrorMessage(manualErrorMsg.replace("#", property));
+        return defaultValue;
+    }
+    return manualAddress;
+}
+
+/**
+ * Get the port value from the NukeServerSocket.ini.
+ *
+ * If NukeServerSocket.ini has a wrong value (no value, boolean value or incorrect port),
+ * will default back on `defaultPort`.
+ *
+ * @param configIni - path of the NukeServerSocket.ini.
+ * @param defaultPort - default port value if ini file had no valid value.
+ * @returns - the port value.
+ */
+function getPortFromIni(configIni: string, defaultPort: string): string {
+    const loadIniFile = require("read-ini-file");
+    const iniContent = loadIniFile.sync(configIni);
+
+    let port = iniContent["server"]["port"];
+
+    if (!port || typeof port !== "string" || !port.match(/\d{5}/)) {
+        const msg = `
+            The configuration for the server/port appears to be invalid.
+            Falling back on port 54321 for now.
+            Try disconnecting and connecting back the server inside Nuke.
             `;
-
-            vscode.window.showErrorMessage(
-                manualErrorMsg.replace('#', config)
-            );
-            return field;
-        }
-        return extField;
-    }
-    return field;
-
-}
-
-function getPort(): string {
-
-
-    let port = '54321';
-    const configIni = getNukeIni();
-
-    if (require('fs').existsSync(configIni)) {
-        const loadIniFile = require('read-ini-file');
-        const iniContent = loadIniFile.sync(configIni);
-
-        let errMsg;
-        try {
-            port = iniContent['server']['port'];
-        } catch (err: unknown) {
-            // server/port doesn't exists?
-            errMsg = err;
-        } finally {
-            // port could be empty or be a boolean if it has no value assigned
-            if (!port || errMsg || typeof port === 'boolean') {
-                port = '54321';
-                const msg = `
-                The configuration for the server/port appears to be invalid.
-                Falling back on port 54321 for now.
-                Try disconnecting and connecting back the server inside Nuke.
-                Error: ${errMsg}
-                `;
-                vscode.window.showErrorMessage(msg);
-            }
-        }
+        vscode.window.showErrorMessage(msg);
+        return defaultPort;
     }
 
-    port = checkManualConnection(port, 'port');
-
-    console.log(`port: ${port}`);
     return port;
-
 }
 
-function getHost() {
-    let host = '127.0.0.1';
+/**
+ * Get the port configuration value.
+ *
+ * The order of which the port value is taken is as follows:
+ *  1. Create a default port value of: 54321
+ *  2. If NukeServerSocket.ini has a valid port value, will be taken instead.
+ *  3. If manual configuration is enabled, will be taken instead.
+ *
+ * If step 2 or 3 will fail, will default back on step 1.
+ *
+ * @returns - port value for the connection.
+ */
+export function getPort(): string {
+    let port = "54321";
 
-    host = checkManualConnection(host, 'host');
+    const configIni = getNukeIni();
+    if (require("fs").existsSync(configIni)) {
+        port = getPortFromIni(configIni, port);
+    }
 
-    console.log(`host: ${host}`);
+    if (utils.getConfig("network.enableManualConnection")) {
+        port = getManualAddress("port", port);
+    }
+    return port;
+}
+
+/**
+ * Get the host address.
+ *
+ * Default host address will be local: 127.0.0.1. If manual connection is enabled
+ * value will be updated from the configuration value.
+ *
+ * @returns - host address
+ */
+function getHost(): string {
+    let host = "127.0.0.1";
+
+    if (utils.getConfig("network.enableManualConnection")) {
+        host = getManualAddress("host", host);
+    }
+
     return host;
 }
 
-// Display addresses to user 
-export function getAddresses() {
+/**
+ * Get the network addresses.
+ *
+ * This will create a simple string: `host: "hostname" port: "port"`
+ *
+ * @returns - string with the network addresses information.
+ */
+export function getAddresses(): string {
     return `host: ${getHost()} port: ${getPort()}`;
 }
 
@@ -119,42 +168,53 @@ async function connectionError(err: Error) {
     If manual connection is enable, verify that the port and host address are correct.
     `;
 
-    const showMessage = await vscode.window.showErrorMessage(msg, 'Show Error');
+    const showMessage = await vscode.window.showErrorMessage(msg, "Show Error");
 
     if (showMessage) {
         vscode.window.showErrorMessage(err.message);
     }
 }
 
+/**
+ * Send data over TCP network.
+ *
+ * @param text - Stringified text to be sent as code to be executed inside Nuke.
+ */
 export function sendText(text: string) {
     let client = new net.Socket();
 
-    // XXX: server appears to connected by default to localhost even when undefined is supplied.
+    // server connects by default to localhost even when undefined is supplied.
     client.connect(getPort(), getHost(), function () {
-        console.log('socketClient -> Connected');
+        console.log("Socket -> Connected");
         client.write(text);
     });
 
-    client.on('lookup', function (error: Error | null, address: string, family: string, host: string) {
+    client.on(
+        "lookup",
+        function (
+            error: Error | null,
+            address: string,
+            family: string,
+            host: string
+        ) {
+            console.log("Socket -> error ::", error);
+            console.log("Socket -> address ::", address);
+            console.log("Socket -> family ::", family);
+            console.log("Socket -> host ::", host);
+        }
+    );
 
-        console.log('socketClient -> error ::', error);
-        console.log('socketClient -> address ::', address);
-        console.log('socketClient -> family ::', family);
-        console.log('socketClient -> host ::', host);
-    });
-
-
-    client.on('error', function (error: Error) {
+    client.on("error", function (error: Error) {
         connectionError(error);
     });
 
-    client.on('data', function (data: Buffer | string) {
+    client.on("data", function (data: Buffer | string) {
         // Encoding of data is set by socket.setEncoding().
 
-        console.log(`socketClient -> Received :: ${data} of type ${typeof data}`);
+        console.log(`Socket -> Received :: ${data} of type ${typeof data}`);
         client.destroy(); // kill client after server's response
 
-        const clearOutput = utils.getConfiguration('other.clearPreviousOutput');
+        const clearOutput = utils.getConfig("other.clearPreviousOutput");
         if (clearOutput) {
             output.clear();
         }
@@ -167,12 +227,11 @@ export function sendText(text: string) {
         output.show(true);
     });
 
-    client.on('close', function (hadError: boolean) {
-        console.log('SocketClient -> Connection closed -> Had Errors? ::', hadError);
+    client.on("close", function (hadError: boolean) {
+        console.log("Socket -> Connection closed. Had Errors? ::", hadError);
     });
 
-    client.on('end', function () {
-        console.log('SocketClient -> Connection ended');
+    client.on("end", function () {
+        console.log("Socket -> Connection ended");
     });
-
 }
