80c32798351473ee633b6b5eda0c106ca0237982
virgilsisoe <virgilsisoe@gmail.com>
2021-11-03 23:56:03 -0400

    test utility functions

---
+++ src/test/suite/utils.ts
@@ -0,0 +1,52 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import { writeFileSync, existsSync, readFileSync } from "fs";
+
+/**
+ * Some tests will need to wait for vscode to register the actions. An example will
+ * be creating/killing terminals and configuration update.
+ *
+ * @param milliseconds - time to sleep
+ * @returns
+ */
+export const sleep = (milliseconds: number) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+/**
+ * Configuration changes require async/await operation to let vscode register
+ * the action.
+ *
+ * @param name - name of the configuration property to update.
+ * @param value - the new value for the property.
+ */
+export async function updateConfig(name: string, value: any) {
+    vscode.extensions.getExtension("virgilsisoe.nuke-tools")?.activate();
+    const nukeTools = vscode.workspace.getConfiguration("nukeTools");
+    await nukeTools.update(name, value, vscode.ConfigurationTarget.Workspace);
+}
+
+/**
+ *
+ * @returns
+ */
+export function tmpFolder(_path?: string) {
+    const cwd = vscode.extensions.getExtension(
+        "virgilsisoe.nuke-tools"
+    )!.extensionPath;
+    if (_path) {
+        return path.join(cwd, "tmp", _path);
+    }
+    return path.join(cwd, "tmp");
+}
+
+/**
+ * Clean the settings.json file inside the temporary folder.
+ */
+export async function cleanSettings() {
+    const settings = path.join(tmpFolder(), ".vscode", "settings.json");
+    if (existsSync(settings)) {
+        writeFileSync(settings, "{}");
+    }
+    await sleep(200);
+}
