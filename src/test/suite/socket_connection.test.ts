981ab29d6b6da7d8e62bac4bb8ed138dc71d5d03
virgilsisoe <virgilsisoe@gmail.com>
2021-11-05 22:15:17 -0400

    test connection first commit

---
+++ src/test/suite/socket_connection.test.ts
@@ -0,0 +1,60 @@
+import * as assert from "assert";
+import * as path from "path";
+
+import { writeFileSync } from "fs";
+
+import * as socket from "../../socket";
+import * as utils from "./utils";
+import * as testUtils from "./utils";
+
+import { Server, Socket } from "net";
+import { Range } from "vscode";
+
+async function server(port: number = 54321, host: string = "localhost") {
+    console.log("init server");
+
+    const _server = new Server();
+    _server.listen(port, host);
+
+    await testUtils.sleep(100);
+
+    _server.on("connection", function (socket) {
+        socket.write("Hello,yo.");
+
+        socket.on("data", function (chunk) {
+            console.log(`Server :: Data received from client: ${chunk.toString()}`);
+        });
+
+        socket.on("end", function () {
+            console.log("Server :: Closing connection with the client");
+            _server.close();
+        });
+    });
+
+    _server.on("close", () => {
+        console.log("Server :: Closing server.");
+    });
+}
+
+suite.only("Send Data", () => {
+    test("Send data to server", async () => {
+        await server();
+        const data = await socket.sendData("localhost", 54321, "hello");
+        assert.strictEqual(data, "Message Received");
+    });
+
+    test("Send data but server is not connected", async () => {
+        const data = await socket.sendData("localhost", 54321, "hello");
+        assert.strictEqual(data, "Connection refused");
+    });
+
+    test("Send data but port is out of range", async () => {
+        assert.throws(() => {
+            socket.sendData("localhost", 124567, "hello");
+        }, RangeError);
+    });
+
+    test.skip("Send data but host is unreachable", async () => {
+        const c = await socket.sendData("192.168.1.99", 12456, "hello");
+    });
+});
