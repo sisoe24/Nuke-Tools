0f84002687eff06c3f092d21d765bb0b825cd2f7
virgilsisoe <virgilsisoe@gmail.com>
2021-11-07 21:39:46 -0500

    refactored socket tests

--- src/test/suite/socket_connection.test.ts
+++ src/test/suite/socket_connection.test.ts
@@ -16,7 +16,7 @@ async function server(port = 54321, host = "localhost") {
     await testUtils.sleep(100);
 
     _server.on("connection", function (socket) {
-        socket.write("Hello,yo.");
+        socket.write("Hello, server.");
 
         socket.on("data", function (chunk) {
             console.log(`Server :: Data received from client: ${chunk.toString()}`);
@@ -33,25 +33,35 @@ async function server(port = 54321, host = "localhost") {
     });
 }
 
+// TODO: write more tests for the server but need more time to plan it
 suite("Send Data", () => {
+    const data = JSON.stringify({ text: "hello", file: "random.file" });
+
     test("Send data to server", async () => {
         await server();
-        const data = await socket.sendData("localhost", 54321, "hello");
-        assert.strictEqual(data, "Message Received");
+        const received = await socket.sendData("localhost", 54321, data);
+        assert.strictEqual(received.error, false);
+        assert.strictEqual(received.message, "Hello, server.");
     });
 
     test("Send data but server is not connected", async () => {
-        const data = await socket.sendData("localhost", 54321, "hello");
-        assert.strictEqual(data, "Connection refused");
+        const received = await socket.sendData("localhost", 54321, data);
+        assert.strictEqual(received.message, "Connection refused");
+        assert.strictEqual(received.error, true);
     });
 
     test("Send data but port is out of range", async () => {
-        assert.throws(() => {
-            socket.sendData("localhost", 124567, "hello");
-        }, RangeError);
+        const received = await socket.sendData("localhost", 654321, data);
+        assert.strictEqual(received.message, "Connection refused");
+        assert.strictEqual(received.error, true);
+        assert.strictEqual(received.errorMessage, "Port is out of range");
     });
 
-    test.skip("Send data but host is unreachable", async () => {
-        await socket.sendData("192.168.1.99", 12456, "hello");
+    test("Send data but host is unreachable", async () => {
+        const received = await socket.sendData("1982.168.1.99", 12456, "hello");
+
+        assert.strictEqual(received.error, true);
+        assert.match(received.errorMessage, /getaddrinfo ENOTFOUND/);
+        assert.strictEqual(received.message, "Connection refused");
     });
 });
