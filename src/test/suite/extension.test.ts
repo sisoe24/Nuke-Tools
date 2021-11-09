63b1b48107c9d6d611bbe18a9b423b7a5b65d15b
virgilsisoe <virgilsisoe@gmail.com>
2021-11-09 12:24:20 -0500

    re added test to check valid configurations

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -2,7 +2,18 @@ import * as assert from "assert";
 import * as utils from "../../utils";
 
 suite("NukeToolsConfig", () => {
-    test("Get invalid value", () => {
+    test("Get a string valid configuration", () => {
+        assert.strictEqual(
+            typeof utils.nukeToolsConfig("nukeExecutable.primaryExecutablePath"),
+            "string"
+        );
+    });
+
+    test("Get a boolean valid configuration", () => {
+        assert.strictEqual(typeof utils.nukeToolsConfig("network.debug"), "boolean");
+    });
+
+    test("Get invalid configuration", () => {
         assert.throws(() => {
             utils.nukeToolsConfig("maya");
         }, Error);
