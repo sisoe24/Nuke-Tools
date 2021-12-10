7f18734b4113093b75885690273475a4e434125f
virgilsisoe <virgilsisoe@gmail.com>
2021-12-10 11:57:24 -0500

    docs

--- src/blinkscript/blink_format.ts
+++ src/blinkscript/blink_format.ts
@@ -1,17 +1,49 @@
 import * as vscode from "vscode";
 
+/**
+ * Clean empty lines.
+ *
+ * @param text text to format
+ * @returns formatted text
+ */
 export function cleanEmptyLines(text: string): string {
     return text.replace(/^\s+$/gm, "");
 }
 
+/**
+ * Clean multiple spaces.
+ *
+ * @param text text to format
+ * @returns formatted text
+ */
 export function cleanMultiSpace(text: string): string {
     return text.replace(/(?<=[^ \n]) {2,}/gm, " ");
 }
 
+/**
+ * Un-indent bracket.
+ *
+ * `if ()`
+ *
+ * `{`
+ *
+ * to
+ *
+ * `if () {`
+ *
+ * @param text text to format
+ * @returns formatted text
+ */
 export function unindentBlock(text: string): string {
     return text.replace(/\s*{/gm, " {");
 }
 
+/**
+ * Format BlinkScript file.
+ *
+ * @param text text to format
+ * @returns formatted text
+ */
 export function formatFile(text: string): string {
     text = cleanEmptyLines(text);
     text = cleanMultiSpace(text);
@@ -28,6 +60,6 @@ export class BlinkScriptFormat implements vscode.DocumentFormattingEditProvider 
         const lines = document.lineCount;
         const text = formatFile(document.getText());
 
-        return [vscode.TextEdit.replace(new vscode.Range(0, 0, lines, 0), text)];
+        return [vscode.TextEdit.replace(new vscode.Range(0, 0, lines - 1, 0), text)];
     }
 }
