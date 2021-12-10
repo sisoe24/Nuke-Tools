e5c511c904c0c9bc07cb2edeb0444e71955010de
virgilsisoe <virgilsisoe@gmail.com>
2021-12-10 11:57:18 -0500

    clean coverage at each run

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -4,6 +4,7 @@ import * as path from "path";
 import * as Mocha from "mocha";
 import * as NYC from "nyc";
 import * as glob from "glob";
+import { rmdirSync } from "fs";
 
 // Simulates the recommended config option
 // extends: "@istanbuljs/nyc-config-typescript",
@@ -56,15 +57,16 @@ export async function run(): Promise<void> {
     // Debug which files will be included/excluded
     // console.log('Glob verification', await nyc.exclude.glob(nyc.cwd));
 
+    // XXX: coverage doesn't seem to update each run so I am deleting the folder
+    rmdirSync(path.join(nyc.cwd, nyc._tempDirectory), { recursive: true });
+
     await nyc.createTempDirectory();
     // Create the mocha test
     const mocha = new Mocha({
         ui: "tdd",
         timeout: 10 * 1000,
+        color: true,
     });
-
-    // Color the stdout
-    mocha.options.color = true;
 
     // Add all files to the test suite
     const files = glob.sync("**/*.test.js", { cwd: testsRoot });
