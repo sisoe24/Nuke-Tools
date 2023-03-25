4b60bfbe9613dd1f8423df94b00cbcb7565b82bb
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-03-25 14:59:47 -0400

    Remove pyside2-template files

--- include/pyside2-template/src/local/__init__.py
+++
@@ -1,1 +0,0 @@
-from .local_nuke import *
--- include/pyside2-template/src/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-from .color_widgets import color_widget
-
-
-tmp_utils = os.path.join(os.path.dirname(__file__), '.tmp')
-if not os.path.exists(tmp_utils):
-    os.mkdir(tmp_utils)
