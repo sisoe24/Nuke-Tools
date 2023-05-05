0823d9ad77c4d1f3b2fcc73eda55e592e0ec4e5e
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-05-05 13:54:35 -0400

    Update description

--- src/stubs.ts
+++ src/stubs.ts
@@ -140,9 +140,10 @@ export async function addStubsPath(): Promise<boolean> {
     }
 
     vscode.window.showInformationMessage(`
-    Python stubs added. You might need to reload the window for the stubs to work. 
-    If you feel that stubs are still not working correctly, you need to update the
-    "python.analysis.packageIndexDepths" setting—more on the extension [README](https://github.com/sisoe24/Nuke-Tools#131-stubs-not-working-correctly).`);
+    Python stubs added.
+    If the stubs are not working properly, update the "python.analysis.packageIndexDepths" setting. 
+    More information is available in the extension README at https://github.com/sisoe24/Nuke-Tools#131-stubs-not-working-correctly.`
+    );
 
     const config = vscode.workspace.getConfiguration(getAutoCompleteSetting());
     const extraPaths = config.get("extraPaths") as Array<string>;
