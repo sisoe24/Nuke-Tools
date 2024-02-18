d3b4e20a9ffcc68a87462327bd6f86adb77380d6
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-02-18 12:16:54 -0500

    Add menu import and cleanup legacy NukeServerSocket import in menu.py

--- src/nuke.ts
+++ src/nuke.ts
@@ -4,23 +4,53 @@ import * as path from "path";
 import * as packages from "./packages";
 import { NUKE_DIR } from "./constants";
 
-export function addMenuImport(importText: string): void {
-    const menuPy = path.join(NUKE_DIR, "menu.py");
+const MENU_PY = path.join(NUKE_DIR, "menu.py");
 
-    if (fs.existsSync(menuPy)) {
-        const fileContent = fs.readFileSync(menuPy, "utf-8");
+/**
+ * Add a menu import to the menu.py file in the .nuke folder.
+ * 
+ * Only adds the import if it doesn't already exist.
+ * 
+ * @param importText The import statement to add to the menu.py file.
+ */
+export function addMenuImport(importText: string): void {
+    if (fs.existsSync(MENU_PY)) {
+        const fileContent = fs.readFileSync(MENU_PY, "utf-8");
         if (!fileContent.includes(importText)) {
-            fs.appendFileSync(menuPy, `\n${importText}\n`);
+            fs.appendFileSync(MENU_PY, `\n${importText}\n`);
         }
     } else {
-        fs.writeFileSync(menuPy, importText);
+        fs.writeFileSync(MENU_PY, importText);
     }
+}
+
+/**
+ * Cleanup the legacy NukeServerSocket import from the menu.py file. 
+ * 
+ * NukeServerSocket < 1.0.0 used to have a different import statement which is now deprecated
+ * and should be removed from the menu.py file if it exists.
+ */
+function cleanupLegacyNukeServerSocket(): void {
+    if (!fs.existsSync(MENU_PY)) {
+        return;
+    }
+
+    const fileContent = fs.readFileSync(MENU_PY, "utf-8");
+
+    const legacyImport = "from NukeTools import NukeServerSocket";
+
+    if (!fileContent.includes(legacyImport)) {
+        return;
+    }
+
+    fs.writeFileSync(MENU_PY, fileContent.replace(legacyImport, ""));
 }
 
 /**
  * Add nukeserversocket to the .nuke folder and import it inside the menu.py
  */
 export function addNukeServerSocket(): void {
+    cleanupLegacyNukeServerSocket();
     packages.addPackage(packages.PackageIds.nukeServerSocket);
     addMenuImport(
         "from NukeTools.nukeserversocket import nukeserversocket\nnukeserversocket.install_nuke()"
