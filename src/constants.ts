a89c8558c28f37ee723e851de56bc1199fc6fbd4
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-02-18 11:01:37 -0500

    Update ASSETS_LOG_PATH in constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -10,9 +10,10 @@ export const NUKE_TOOLS_DIR = path.join(NUKE_DIR, "NukeTools");
 export const PACKAGE_ROOT  = vscode.extensions.getExtension("virgilsisoe.nuke-tools")
     ?.extensionPath as string;
 
-export const ASSETS_PATH = path.join(PACKAGE_ROOT, "resources", "assets");
+export const PACKAGE_RESOURCES_PATH = path.join(PACKAGE_ROOT, "resources");
+export const ASSETS_PATH = path.join(PACKAGE_RESOURCES_PATH, "assets");
 if (!fs.existsSync(ASSETS_PATH )) {
     fs.mkdirSync(ASSETS_PATH );
 }
 
-export const ASSETS_LOG_PATH = path.join(PACKAGE_ROOT, "github_packages.json");
+export const ASSETS_LOG_PATH = path.join(PACKAGE_RESOURCES_PATH, "included_assets.json");
