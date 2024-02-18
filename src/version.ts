328b38e5d6b864fa29c29ff0eacf6d5427105e3f
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-02-18 09:48:12 -0500

    Add version tracking and package version retrieval

--- src/version.ts
+++ src/version.ts
@@ -1,20 +1,34 @@
 import * as vscode from "vscode";
+import * as fs from "fs";
+import { ASSETS_LOG_PATH } from "./constants";
 
 export class Version {
+    private static _extPreviousVersion: string;
+    private static _extCurrentVersion: string;
 
-    private static _previousVersion: string;
-    private static _currentVersion: string;
-
-    public static get previousVersion(): string {
-        return Version._previousVersion;
+    /**
+     * Get the previous version of the extension.
+     */
+    public static get extPreviousVersion(): string {
+        return Version._extPreviousVersion;
     }
 
-    public static get currentVersion(): string {
-        return Version._currentVersion;
+    /**
+     * Get the current version of the extension.
+     */
+    public static get extCurrentVersion(): string {
+        return Version._extCurrentVersion;
     }
 
     public static update(cxt: vscode.ExtensionContext): void {
-        Version._previousVersion = (cxt.globalState.get(cxt.extension.id + ".version") as string) || "0.0.0";
-        Version._currentVersion = cxt.extension.packageJSON.version;
+        Version._extPreviousVersion =
+            (cxt.globalState.get(cxt.extension.id + ".version") as string) || "0.0.0";
+        Version._extCurrentVersion = cxt.extension.packageJSON.version;
+    }
+
+    public static getPackageVersion(packageName: string): string {
+        const logFile = fs.readFileSync(ASSETS_LOG_PATH, "utf8");
+        const data = JSON.parse(logFile);
+        return data["packages"][packageName];
     }
 }
